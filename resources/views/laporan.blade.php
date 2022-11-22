0b64df64327a3394990c831dfcf4627fa34a78f3
hendr <hendriyawan0711@gmail.com>
2022-11-22 16:10:39 +0700

    new

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -1,1 +1,3 @@
-<h2>LAPORAN</h2>
+<h2>LAPORAN
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis alias aspernatur recusandae enim nisi! Quos quo neque illo natus dolorum voluptatem magnam iste laudantium, quisquam blanditiis ea error. Itaque, voluptas!
+</h2>
